948b9bfdc29603703d924b90bcbdfc8a1b1c0fb4
Dong Kyu Kim <dongkyukim00@gmail.com>
2021-02-23 10:08:52 -0600

    Updated routes and templates

---
+++ README.md
@@ -0,0 +1,5 @@
+# Mini message board
+
+A basic message board created using Node and Express.js.
+
+[View demo]()
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+const title = 'Mini Messageboard';
+
 const messages = [
    {
      text: "Hi there!",
@@ -16,7 +18,21 @@ const messages = [
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title, messages });
+});
+
+/* GET form listing. */
+router.get('/new', function(req, res, next) {
+  res.render('form', {title});
+});
+
+router.post('/new', function(req, res, next) {
+  const messageText = req.body.messageText;
+  const messageUser = req.body.messageUser;
+
+  // Create new date object
+  messages.push({text: messageText, user: messageUser, added: new Date()})
+  res.redirect('/');
 });
 
 module.exports = router;
